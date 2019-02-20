2d215a2d326b97261c627e1acb0dbd5522d8a7b1
davidtoltesy <davidtoltesy@davidtoltesys-MacBook-Pro.local>
2019-02-20 17:31:48 +0100

    Update cachematrix.R
    Debug() results

--- cachematrix.R
+++ cachematrix.R
@@ -56,3 +56,39 @@ cacheSolve <- function(x, ...) {
     x$setInverse(inverseCache)
         inverseCache
 }
+
+##-----Debuging the program-----
+
+## > debug(cacheSolve)
+## > cacheSolve(m1)
+## debugging in: cacheSolve(m1)
+## debug at ~/Documents/GitHub/ProgrammingAssignment2/cachematrix.R#40: {
+##    inverseCache <- x$getInverse()
+##    if (!is.null(inverseCache)) {
+##        message("Getting the cached data")
+##        return(inverseCache)
+##    }
+##    data <- x$getMatrix()
+##    inverseCache <- solve(data, ...)
+##    x$setInverse(inverseCache)
+##    inverseCache
+##}
+## Browse[2]>
+## debug at ## ~/Documents/GitHub/ProgrammingAssignment2/cachematrix.R#44: inverseCache <- x$getInverse()
+## Browse[2]>
+## debug at ~/Documents/GitHub/ProgrammingAssignment2/cachematrix.R#47: if (!is.null(inverseCache)) {
+##    message("Getting the cached data")
+##    return(inverseCache)
+## }
+## Browse[2]>
+## debug at ~/Documents/GitHub/ProgrammingAssignment2/cachematrix.R#48: message("Getting the cached data")
+## Browse[2]>
+## Getting the cached data
+## debug at ~/Documents/GitHub/ProgrammingAssignment2/cachematrix.R#49: return(inverseCache)
+## Browse[2]>
+## exiting from: cacheSolve(m1)
+##           [,1]       [,2]       [,3]       [,4]
+## [1,] -1.103614  0.4619003 -0.8468213  0.1603883
+## [2,]  4.953622 -4.1491442  3.1038958  1.6461716
+## [3,] -8.249267  6.7118825 -6.1928336 -2.7668901
+## [4,]  4.932770 -4.8247051  3.8629930  1.8299655 
